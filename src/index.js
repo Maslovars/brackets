a51ed1ecd16e449695717ab5b4c1d1c7dec6a745
Maslovars <maslovars@gmail.com>
2021-08-30 22:29:15 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+
+  let arr = [];
+  for (const char of str) {
+    if (arr[arr.length - 1] === char) arr.pop();
+    else {
+      const pair = bracketsConfig.find(pair => pair[0] === char);
+      if (pair) arr.push(pair[1]);
+      else {
+        return false;
+      }
+    }
+  }
+
+  return !arr.length;
 }
